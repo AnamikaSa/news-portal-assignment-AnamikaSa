0e2932ee1a422c11324991566f497b0ab8320a7b
AnamikaSa <amanika0061@gmail.com>
2022-09-03 20:32:27 +0600

    added some design

--- news.js
+++ news.js
@@ -18,33 +18,46 @@ const displayCatagory=news=>{
 }
 
 const forNews =async()=>{
-    const r= await fetch(" https://openapi.programming-hero.com/api/news/category/01");
+    const r= await fetch(" https://openapi.programming-hero.com/api/news/category/04");
     const d= await r.json();
     console.log(d);
     displayNewsDetails(d.data);
 }
 const displayNewsDetails=Details=>{
     const newsDe= document.getElementById('news-details');
+    newsDe.textContent='';
     Details.forEach(newDetails =>{
         // console.log(newCatagories);
         const div =document.createElement('div');
         div.innerHTML=`
-        <div class="ml-5 w-80 d-flex>
-        <div class="h-48 w-full  rounded overflow-hidden" >
-        <image src="${newDetails.image_url}">
+        <div class="  mt-14 lg:max-w-full lg:flex">
+        <div class=" lg:h-auto lg:w-96 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+            <image src="${newDetails.image_url}" class="h-64"></image>
         </div>
-           <div class=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between">
+        <div class=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+          <div class="mb-8">
             <div class="text-gray-900 font-bold text-xl mb-2">${newDetails.title}</div>
-            <p class="text-gray-700 text-base">${newDetails.details}</p>
+            <p id="para" class="text-gray-700 text-base">${newDetails.details}</p>
           </div>
-          <div>
-            <img class="ml-5 w-10 h-10 rounded-full" src="${newDetails.author.img}">
-            <div class="ml-5 text-sm">
-              <p class="text-gray-900 leading-none">${newDetails.author.name}</p>
-              <p class="text-gray-600">Aug 18</p>
+          <div class="flex items-center">
+            <img class="w-10 h-10 rounded-full" src="${newDetails.author.img}" >
+            <div class="text-sm mt-2">
+              <p class="text-gray-900 leading-none pl-3">${newDetails.author.name}</p>
+              <p class="pl-3">10th Jan, 2022</p>
+            </div>
+            
+            <div class="mx-auto flex">
+            <div class="flex ml-20">
+            <i class="fa-solid fa-eye mt-3"></i>
+            <p class="mt-2 ml-2">${newDetails.total_view}</p>
+            </div>
+            <div>
+            <button class="bg-sky-400 p-3 rounded ml-72">Show Details</button>
             </div>
             </div>
+          </div>
         </div>
+      </div>
         `;
         newsDe.appendChild(div);
 
