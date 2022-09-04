f4c249f3aee7d471f07115d9e4364c984712c154
AnamikaSa <amanika0061@gmail.com>
2022-09-04 09:37:45 +0600

    had to change to bootstrap because of modal

--- news.js
+++ news.js
@@ -1,24 +1,28 @@
 const loadNews = async() => {
-    const r= await fetch("https://openapi.programming-hero.com/api/news/categories");
+    const r= await fetch(`https://openapi.programming-hero.com/api/news/categories`);
     const d= await r.json();
     // console.log(d);
     displayCatagory(d.data.news_category);
 }
+
+
 const displayCatagory=news=>{
     const newsCata= document.getElementById('news-catagories');
+    
     news.forEach(newCatagories =>{
         // console.log(newCatagories);
         const li =document.createElement('li');
         li.innerHTML=`
-        <a onclick=forNews() href="#" class="hover:bg-sky-300 p-4 rounded">${newCatagories.category_name}</a>
+        <a onclick=forNews() href="#" class="hover:bg-sky-300 p-2 rounded">${newCatagories.category_name}</a>
         `;
         newsCata.appendChild(li);
+        
 
     });
 }
 
 const forNews =async()=>{
-    const r= await fetch(" https://openapi.programming-hero.com/api/news/category/04");
+    const r= await fetch(`https://openapi.programming-hero.com/api/news/category/04`);
     const d= await r.json();
     console.log(d);
     displayNewsDetails(d.data);
@@ -27,7 +31,8 @@ const displayNewsDetails=Details=>{
     const newsDe= document.getElementById('news-details');
     newsDe.textContent='';
     Details.forEach(newDetails =>{
-        // console.log(newCatagories);
+        // console.log(newDetails.length);
+        // if(newDetails.data.length)
         const div =document.createElement('div');
         div.innerHTML=`
         <div class="  mt-14 lg:max-w-full lg:flex">
@@ -52,7 +57,7 @@ const displayNewsDetails=Details=>{
             <p class="mt-2 ml-2">${newDetails.total_view}</p>
             </div>
             <div>
-            <button class="bg-sky-400 p-3 rounded ml-72">Show Details</button>
+            <button type="button" onclick="loadDetails('${newDetails.details}')" href="#" class="bg-sky-400 text-gray-900 p-3 rounded ml-72" data-bs-toggle="modal" data-bs-target="#D">Show Details</button>
             </div>
             </div>
           </div>
@@ -62,6 +67,25 @@ const displayNewsDetails=Details=>{
         newsDe.appendChild(div);
 
     });
+}
+
+const loadDetails = async ()=>{
+  const url =`https://openapi.programming-hero.com/api/news/category/04`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayDetails(data.data);
+}
+
+const displayDetails = n =>{
+  const modalTitle = document.getElementById('D');
+  modalTitle.innerText = newDetails.title;
+  n.forEach(newDetails=>{
+  const newsDetailsModals = document.getElementById('news-details_modals');
+  // console.log(phone.mainFeatures.sensors[0]);
+  newsDetailsModals.innerHTML=`
+      <p> ${newDetails.details} </p>
+  `
+});
 }
 forNews();
 loadNews();
