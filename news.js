991d7da5d41d228242b43256dc787919e0eaab96
AnamikaSa <amanika0061@gmail.com>
2022-09-04 10:34:33 +0600

    modal added with changing fetch outherwise could not added the link dynamically

--- news.js
+++ news.js
@@ -1,19 +1,21 @@
-const loadNews = async() => {
-    const r= await fetch(`https://openapi.programming-hero.com/api/news/categories`);
-    const d= await r.json();
-    // console.log(d);
-    displayCatagory(d.data.news_category);
+const loadNews = () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`
+  fetch(url)
+      .then(res => res.json())
+      .then(data => displayCatagories(data.data.news_category))
+      .catch(error => console.log(error))
 }
 
 
-const displayCatagory=news=>{
+
+const displayCatagories=news=>{
     const newsCata= document.getElementById('news-catagories');
     
     news.forEach(newCatagories =>{
         // console.log(newCatagories);
         const li =document.createElement('li');
         li.innerHTML=`
-        <a onclick=forNews() href="#" class="hover:bg-sky-300 p-2 rounded">${newCatagories.category_name}</a>
+        <a onclick=forNewsOfAll('${newCatagories.category_id}') href="#" class=" p-2 rounded text-decoration-none list-group-item list-group-item-action list-group-item-primary">${newCatagories.category_name}</a>
         `;
         newsCata.appendChild(li);
         
@@ -21,71 +23,81 @@ const displayCatagory=news=>{
     });
 }
 
-const forNews =async()=>{
-    const r= await fetch(`https://openapi.programming-hero.com/api/news/category/04`);
-    const d= await r.json();
-    console.log(d);
-    displayNewsDetails(d.data);
+const forNewsOfAll = (id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+  fetch(url)
+      .then(res => res.json())
+      .then(data => displayNewsDetails(data.data))
+      .catch(error => console.log(error))
 }
-const displayNewsDetails=Details=>{
+
+
+const displayNewsDetails=CategoryDetails=>{
     const newsDe= document.getElementById('news-details');
     newsDe.textContent='';
-    Details.forEach(newDetails =>{
+    CategoryDetails.forEach(newDetails =>{
         // console.log(newDetails.length);
         // if(newDetails.data.length)
         const div =document.createElement('div');
         div.innerHTML=`
-        <div class="  mt-14 lg:max-w-full lg:flex">
-        <div class=" lg:h-auto lg:w-96 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
-            <image src="${newDetails.image_url}" class="h-64"></image>
+        <div class="row" onclick="loadDetails('${newDetails._id}')">
+            <div class="border rounded mb-5 pt-3 pb-3 d-flex shadow">
+                <div class="col-md-3 col-5">
+                    <img src="${newDetails.thumbnail_url}" class="img-fluid h-100 rounded">
+                </div>
+                <div class="col-md-9 col-7 ps-4">
+                    <div class="card-body p-0 d-flex flex-column">
+                        <div class="mb-5">
+                        <h5 class="card-title">${newDetails.title}</h5>
+                        <p class="card-text  mb-3" id="para">${newDetails.details}</p> 
+                        </div>
+                        <div class="mt-5 d-flex align-items-center justify-content-between justify-content-center">
+                            <div class="d-flex align-items-center justify-content-center">
+                                <div>
+                                    <img  style="height: 70px;" class ="img-fluid mh-100 rounded-circle" src="${newDetails.author.img}">
+                                    <p class="text-capitalize">${newDetails.author.name ? newDetails.author.name : 'Unknown'}</p>
+                                </div>    
+                            </div>
+                            <div class="ps-3 pe-3">
+                                <i class="fa-regular fa-eye"></i>
+                                <p>${newDetails.total_view ? newDetails.total_view : '0'}</p>
+                            </div>
+                            <div>
+                            <button class="btn btn-primary p-2" data-bs-toggle="modal"
+                            data-bs-target="#detailOnModal">Show All-> </button>
+                            </div>
+                        </div>  
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-          <div class="mb-8">
-            <div class="text-gray-900 font-bold text-xl mb-2">${newDetails.title}</div>
-            <p id="para" class="text-gray-700 text-base">${newDetails.details}</p>
-          </div>
-          <div class="flex items-center">
-            <img class="w-10 h-10 rounded-full" src="${newDetails.author.img}" >
-            <div class="text-sm mt-2">
-              <p class="text-gray-900 leading-none pl-3">${newDetails.author.name}</p>
-              <p class="pl-3">10th Jan, 2022</p>
-            </div>
-            
-            <div class="mx-auto flex">
-            <div class="flex ml-20">
-            <i class="fa-solid fa-eye mt-3"></i>
-            <p class="mt-2 ml-2">${newDetails.total_view}</p>
-            </div>
-            <div>
-            <button type="button" onclick="loadDetails('${newDetails.details}')" href="#" class="bg-sky-400 text-gray-900 p-3 rounded ml-72" data-bs-toggle="modal" data-bs-target="#D">Show Details</button>
-            </div>
-            </div>
-          </div>
-        </div>
-      </div>
-        `;
+        `
         newsDe.appendChild(div);
 
     });
 }
 
-const loadDetails = async ()=>{
-  const url =`https://openapi.programming-hero.com/api/news/category/04`;
-  const res = await fetch(url);
-  const data = await res.json();
-  displayDetails(data.data);
+const loadDetails = (_id)=>{
+  const url = `https://openapi.programming-hero.com/api/news/${_id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayDetails(data.data))
+        .catch(error => console.log(error))
 }
 
 const displayDetails = n =>{
-  const modalTitle = document.getElementById('D');
-  modalTitle.innerText = newDetails.title;
+  
+  
   n.forEach(newDetails=>{
+    const modalTitle = document.getElementById('D');
+     modalTitle.innerText = `${newDetails.title}`
   const newsDetailsModals = document.getElementById('news-details_modals');
   // console.log(phone.mainFeatures.sensors[0]);
   newsDetailsModals.innerHTML=`
+    <p> ${newDetails.title} </p>
       <p> ${newDetails.details} </p>
   `
-});
+})
 }
-forNews();
-loadNews();
+// forNews();
+loadNews()
